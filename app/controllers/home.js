0a522f1549467ff46e81cbf7069398c4d926cb1c
Saint <yoyothai00@gmail.com>
2016-05-27 06:39:33 +0700

    init

---
+++ app/controllers/home.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+  router = express.Router(),
+  Article = require('../models/article');
+
+module.exports = function (app) {
+  app.use('/', router);
+};
+
+router.get('/', function (req, res, next) {
+    res.render('index', {
+      title: 'ReverseSearch'
+    });
+});
